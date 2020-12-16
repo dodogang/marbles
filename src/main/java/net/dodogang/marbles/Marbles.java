52e82956673cd3e52977a28f39623efeff2f6406
Daniel Orr <orrdaniel73@gmail.com>
2020-12-16 16:54:37 +0000

    initial commit

---
+++ src/main/java/net/dodogang/marbles/Marbles.java
@@ -0,0 +1,30 @@
+package net.dodogang.marbles;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Marbles implements ModInitializer {
+    public static Logger LOGGER = LogManager.getLogger();
+
+    public static final String MOD_ID = "marbles";
+    public static final String MOD_NAME = "Marbles";
+
+    @Override
+    public void onInitialize() {
+        log("Initializing");
+
+        //
+
+        log("Initialized");
+    }
+
+    public static void log(Level level, String message) {
+        LOGGER.log(level, "[" + MOD_NAME + "] " + message);
+    }
+    public static void log(String message) {
+        log(Level.INFO, message);
+    }
+}
