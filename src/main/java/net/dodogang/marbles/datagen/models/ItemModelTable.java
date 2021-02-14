1ffbd647cd3b9199f1ebc90836d6ce72637f987f
Daniel Orr <orrdaniel73@gmail.com>
2021-02-14 23:19:54 +0000

    reorganise datagen packages

---
+++ src/main/java/net/dodogang/marbles/datagen/models/ItemModelTable.java
@@ -0,0 +1,213 @@
+package net.dodogang.marbles.datagen.models;
+
+import me.andante.chord.block.helper.WoodBlocks;
+import net.dodogang.marbles.datagen.models.modelgen.ModelGen;
+import net.dodogang.marbles.init.MarblesBlocks;
+import net.dodogang.marbles.init.MarblesItems;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
+import static net.dodogang.marbles.datagen.models.modelgen.InheritingModelGen.*;
+
+public final class ItemModelTable {
+    private static BiConsumer<Item, ModelGen> consumer;
+
+    public static void registerItemModels(BiConsumer<Item, ModelGen> c) {
+        consumer = c;
+
+        registerWoodBlocks(MarblesBlocks.ASPEN);
+        registerWoodBlocks(MarblesBlocks.HOOPSI_SPRUCE);
+
+        register(MarblesBlocks.TRAVERTINE, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.TRAVERTINE_BRICKS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CAPPED_TRAVERTINE, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.POLISHED_TRAVERTINE, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.TRAVERTINE_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.TRAVERTINE_BRICK_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CAPPED_TRAVERTINE_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.POLISHED_TRAVERTINE_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.TRAVERTINE_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.TRAVERTINE_BRICK_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CAPPED_TRAVERTINE_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.POLISHED_TRAVERTINE_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.TRAVERTINE_WALL, item -> using(name(item, "block/%s", "_wall"), n -> wallSidedInventory(n + "_top", n + "_top", n)));
+        register(MarblesBlocks.TRAVERTINE_BRICK_WALL, item -> using(name(item, "block/%s_bricks", "_brick_wall"), n -> wallSidedInventory(n + "_top", n + "_top", n)));
+        register(MarblesBlocks.CAPPED_TRAVERTINE_WALL, item -> using(name(item, "block/%s", "_wall"), n -> wallSidedInventory(n + "_top", n + "_top", n)));
+        register(MarblesBlocks.POLISHED_TRAVERTINE_WALL, item -> wallInventory(name(item, "block/%s", "_wall")));
+
+        register(MarblesBlocks.LEMON_TRAVERTINE, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.LEMON_TRAVERTINE_BRICKS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CAPPED_LEMON_TRAVERTINE, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.POLISHED_LEMON_TRAVERTINE, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.LEMON_TRAVERTINE_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.LEMON_TRAVERTINE_BRICK_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CAPPED_LEMON_TRAVERTINE_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.POLISHED_LEMON_TRAVERTINE_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.LEMON_TRAVERTINE_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.LEMON_TRAVERTINE_BRICK_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CAPPED_LEMON_TRAVERTINE_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.POLISHED_LEMON_TRAVERTINE_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.LEMON_TRAVERTINE_WALL, item -> using(name(item, "block/%s", "_wall"), n -> wallSidedInventory(n + "_top", n + "_top", n)));
+        register(MarblesBlocks.LEMON_TRAVERTINE_BRICK_WALL, item -> using(name(item, "block/%s_bricks", "_brick_wall"), n -> wallSidedInventory(n + "_top", n + "_top", n)));
+        register(MarblesBlocks.CAPPED_LEMON_TRAVERTINE_WALL, item -> using(name(item, "block/%s", "_wall"), n -> wallSidedInventory(n + "_top", n + "_top", n)));
+        register(MarblesBlocks.POLISHED_LEMON_TRAVERTINE_WALL, item -> wallInventory(name(item, "block/%s", "_wall")));
+
+        register(MarblesBlocks.PEACH_TRAVERTINE, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.PEACH_TRAVERTINE_BRICKS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CAPPED_PEACH_TRAVERTINE, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.POLISHED_PEACH_TRAVERTINE, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.PEACH_TRAVERTINE_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.PEACH_TRAVERTINE_BRICK_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CAPPED_PEACH_TRAVERTINE_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.POLISHED_PEACH_TRAVERTINE_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.PEACH_TRAVERTINE_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.PEACH_TRAVERTINE_BRICK_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CAPPED_PEACH_TRAVERTINE_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.POLISHED_PEACH_TRAVERTINE_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.PEACH_TRAVERTINE_WALL, item -> using(name(item, "block/%s", "_wall"), n -> wallSidedInventory(n + "_top", n + "_top", n)));
+        register(MarblesBlocks.PEACH_TRAVERTINE_BRICK_WALL, item -> using(name(item, "block/%s_bricks", "_brick_wall"), n -> wallSidedInventory(n + "_top", n + "_top", n)));
+        register(MarblesBlocks.CAPPED_PEACH_TRAVERTINE_WALL, item -> using(name(item, "block/%s", "_wall"), n -> wallSidedInventory(n + "_top", n + "_top", n)));
+        register(MarblesBlocks.POLISHED_PEACH_TRAVERTINE_WALL, item -> wallInventory(name(item, "block/%s", "_wall")));
+
+        register(MarblesBlocks.TANGERINE_TRAVERTINE, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.TANGERINE_TRAVERTINE_BRICKS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CAPPED_TANGERINE_TRAVERTINE, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.POLISHED_TANGERINE_TRAVERTINE, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.TANGERINE_TRAVERTINE_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.TANGERINE_TRAVERTINE_BRICK_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CAPPED_TANGERINE_TRAVERTINE_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.POLISHED_TANGERINE_TRAVERTINE_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.TANGERINE_TRAVERTINE_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.TANGERINE_TRAVERTINE_BRICK_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CAPPED_TANGERINE_TRAVERTINE_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.POLISHED_TANGERINE_TRAVERTINE_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.TANGERINE_TRAVERTINE_WALL, item -> using(name(item, "block/%s", "_wall"), n -> wallSidedInventory(n + "_top", n + "_top", n)));
+        register(MarblesBlocks.TANGERINE_TRAVERTINE_BRICK_WALL, item -> using(name(item, "block/%s_bricks", "_brick_wall"), n -> wallSidedInventory(n + "_top", n + "_top", n)));
+        register(MarblesBlocks.CAPPED_TANGERINE_TRAVERTINE_WALL, item -> using(name(item, "block/%s", "_wall"), n -> wallSidedInventory(n + "_top", n + "_top", n)));
+        register(MarblesBlocks.POLISHED_TANGERINE_TRAVERTINE_WALL, item -> wallInventory(name(item, "block/%s", "_wall")));
+
+        register(MarblesBlocks.TRAVERTINE_OBSIDIAN, item -> inherit(name(item, "block/%s")));
+
+        register(MarblesBlocks.TRAVERTINE_SALT_LAMP, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.LEMON_TRAVERTINE_SALT_LAMP, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.PEACH_TRAVERTINE_SALT_LAMP, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.TANGERINE_TRAVERTINE_SALT_LAMP, item -> inherit(name(item, "block/%s")));
+
+        register(MarblesBlocks.PINK_SALT, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CRUMBLED_PINK_SALT, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.PINK_SALT_SPIRE, item -> generated(name(item, "item/%s")));
+        register(MarblesBlocks.PINK_SALT_STACK, item -> generated(name(item, "item/%s")));
+        register(MarblesBlocks.PINK_SALT_STUMP, item -> generated(name(item, "item/%s")));
+
+        register(MarblesBlocks.LAPIS_SHINGLES, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.LAPIS_SHINGLES_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.LAPIS_SHINGLES_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.LAPIS_SPOTLIGHT, item -> inherit(name(item, "block/%s")));
+
+        register(MarblesBlocks.DAWN_SAND, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.DAWN_SANDSTONE, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CHISELED_DAWN_SANDSTONE, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CUT_DAWN_SANDSTONE, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.SMOOTH_DAWN_SANDSTONE, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.DAWN_SANDSTONE_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CUT_DAWN_SANDSTONE_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.SMOOTH_DAWN_SANDSTONE_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.DAWN_SANDSTONE_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CUT_DAWN_SANDSTONE_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.SMOOTH_DAWN_SANDSTONE_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.DAWN_SANDSTONE_WALL, item -> using(name(item, "block/%s", "_wall"), n -> wallSidedInventory(n + "_bottom", n + "_top", n)));
+        register(MarblesBlocks.CUT_DAWN_SANDSTONE_WALL, item -> using(name(item, "block/%s", "(^cut_)|(_wall$)", ""), n -> wallSidedInventory(n + "_bottom", n + "_top", n)));
+        register(MarblesBlocks.SMOOTH_DAWN_SANDSTONE_WALL, item -> wallInventory(name(item, "block/%s_top", "(^smooth_)|(_wall$)", "")));
+
+        register(MarblesBlocks.DUSK_SAND, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.DUSK_SANDSTONE, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CHISELED_DUSK_SANDSTONE, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CUT_DUSK_SANDSTONE, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.SMOOTH_DUSK_SANDSTONE, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.DUSK_SANDSTONE_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CUT_DUSK_SANDSTONE_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.SMOOTH_DUSK_SANDSTONE_SLAB, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.DUSK_SANDSTONE_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.CUT_DUSK_SANDSTONE_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.SMOOTH_DUSK_SANDSTONE_STAIRS, item -> inherit(name(item, "block/%s")));
+        register(MarblesBlocks.DUSK_SANDSTONE_WALL, item -> using(name(item, "block/%s", "_wall"), n -> wallSidedInventory(n + "_bottom", n + "_top", n)));
+        register(MarblesBlocks.CUT_DUSK_SANDSTONE_WALL, item -> using(name(item, "block/%s", "(^cut_)|(_wall$)", ""), n -> wallSidedInventory(n + "_bottom", n + "_top", n)));
+        register(MarblesBlocks.SMOOTH_DUSK_SANDSTONE_WALL, item -> wallInventory(name(item, "block/%s_top", "(^smooth_)|(_wall$)", "")));
+
+        register(MarblesBlocks.YELLOW_BAMBOO, item -> generated(name(item, "item/%s")));
+
+        register(MarblesBlocks.GRISP_DIRT, item -> inherit(name(item, "block/%s")));
+
+        register(MarblesItems.YELLOW_SCAFFOLDING, item -> inherit("marbles:block/yellow_scaffolding_stable"));
+        register(MarblesItems.PINK_SALT_SHARD, item -> generated(name(item, "item/%s")));
+    }
+
+
+    private static void registerWoodBlocks(WoodBlocks blocks) {
+        register(blocks.PLANKS, item -> inherit(name(item, "block/%s")));
+        register(blocks.SAPLING, item -> generated(name(item, "block/%s")));
+        register(blocks.LOG, item -> inherit(name(item, "block/%s")));
+        register(blocks.STRIPPED_LOG, item -> inherit(name(item, "block/%s")));
+        register(blocks.WOOD, item -> inherit(name(item, "block/%s")));
+        register(blocks.STRIPPED_WOOD, item -> inherit(name(item, "block/%s")));
+        register(blocks.LEAVES, item -> inherit(name(item, "block/%s")));
+        register(blocks.SLAB, item -> inherit(name(item, "block/%s")));
+        register(blocks.STAIRS, item -> inherit(name(item, "block/%s")));
+        register(blocks.FENCE, item -> fenceInventory(name(item, "block/%s_planks", "_fence")));
+        register(blocks.DOOR, item -> generated(name(item, "item/%s")));
+        register(blocks.TRAPDOOR, item -> inherit(name(item, "block/%s_bottom")));
+        register(blocks.FENCE_GATE, item -> inherit(name(item, "block/%s")));
+        register(blocks.PRESSURE_PLATE, item -> inherit(name(item, "block/%s")));
+        register(blocks.BUTTON, item -> buttonInventory(name(item, "block/%s_planks", "_button")));
+        register(blocks.SIGN_ITEM, item -> generated(name(item, "item/%s")));
+        register(blocks.BOAT_ITEM, item -> generated(name(item, "item/%s")));
+    }
+
+    private static void register(ItemConvertible provider, Function<Item, ModelGen> genFactory) {
+        Item item = provider.asItem();
+        ModelGen gen = genFactory.apply(item);
+        consumer.accept(item, gen);
+    }
+
+    private static String name(Item item, String nameFormat) {
+        Identifier id = Registry.ITEM.getId(item);
+        return String.format("%s:%s", id.getNamespace(), String.format(nameFormat, id.getPath()));
+    }
+
+    private static String name(Item item) {
+        Identifier id = Registry.ITEM.getId(item);
+        return id.toString();
+    }
+
+    private static String name(Item item, String nameFormat, String omitSuffix) {
+        Identifier id = Registry.ITEM.getId(item);
+
+        String path = id.getPath();
+        if (path.endsWith(omitSuffix)) {
+            path = path.substring(0, path.length() - omitSuffix.length());
+        }
+
+        return String.format("%s:%s", id.getNamespace(), String.format(nameFormat, path));
+    }
+
+    private static String name(Item item, String nameFormat, String pattern, String reformat) {
+        Identifier id = Registry.ITEM.getId(item);
+
+        String path = id.getPath();
+        path = path.replaceAll(pattern, reformat);
+
+        return String.format("%s:%s", id.getNamespace(), String.format(nameFormat, path));
+    }
+
+    private static <T> T using(String name, Function<String, T> func) {
+        return func.apply(name);
+    }
+
+    private ItemModelTable() {
+    }
+}
---
+++ src/main/java/net/dodogang/marbles/datagen/models/stategen/BuildingBlocks.java
@@ -0,0 +1,184 @@
+package net.dodogang.marbles.datagen.models.stategen;
+
+import net.dodogang.marbles.datagen.models.modelgen.InheritingModelGen;
+import net.dodogang.marbles.datagen.models.modelgen.ModelGen;
+import net.minecraft.block.enums.BlockHalf;
+import net.minecraft.util.math.Direction;
+
+import static net.dodogang.marbles.datagen.models.modelgen.InheritingModelGen.fencePost;
+import static net.dodogang.marbles.datagen.models.modelgen.InheritingModelGen.fenceSide;
+
+public abstract class BuildingBlocks {
+    public static StateGen fence(String name, String texName) {
+        return MultipartBlockStateGen.multipart()
+                                     .part(ModelInfo.create(name + "_post", fencePost(texName)))
+                                     .part(
+                                         Selector.and().condition("north", "true"),
+                                         ModelInfo.create(name + "_side", fenceSide(texName))
+                                     )
+                                     .part(
+                                         Selector.and().condition("east", "true"),
+                                         ModelInfo.create(name + "_side", fenceSide(texName))
+                                                  .rotate(0, 90)
+                                                  .uvlock(true)
+                                     )
+                                     .part(
+                                         Selector.and().condition("south", "true"),
+                                         ModelInfo.create(name + "_side", fenceSide(texName))
+                                                  .rotate(0, 180)
+                                                  .uvlock(true)
+                                     )
+                                     .part(
+                                         Selector.and().condition("west", "true"),
+                                         ModelInfo.create(name + "_side", fenceSide(texName))
+                                                  .rotate(0, 270)
+                                                  .uvlock(true)
+                                     );
+    }
+
+    private static StateGen wallGeneric(String name, ModelGen post, ModelGen side, ModelGen tall) {
+        ModelInfo postC = ModelInfo.create(name + "_post", post);
+        ModelInfo sideN = ModelInfo.create(name + "_side", side).uvlock(true).rotate(0, 0);
+        ModelInfo sideE = ModelInfo.create(name + "_side", side).uvlock(true).rotate(0, 90);
+        ModelInfo sideS = ModelInfo.create(name + "_side", side).uvlock(true).rotate(0, 180);
+        ModelInfo sideW = ModelInfo.create(name + "_side", side).uvlock(true).rotate(0, 270);
+        ModelInfo sideTallN = ModelInfo.create(name + "_side_tall", tall).uvlock(true).rotate(0, 0);
+        ModelInfo sideTallE = ModelInfo.create(name + "_side_tall", tall).uvlock(true).rotate(0, 90);
+        ModelInfo sideTallS = ModelInfo.create(name + "_side_tall", tall).uvlock(true).rotate(0, 180);
+        ModelInfo sideTallW = ModelInfo.create(name + "_side_tall", tall).uvlock(true).rotate(0, 270);
+
+        return MultipartBlockStateGen.multipart()
+                                     .part(Selector.and().condition("up", "true"), postC)
+                                     .part(Selector.and().condition("north", "low"), sideN)
+                                     .part(Selector.and().condition("east", "low"), sideE)
+                                     .part(Selector.and().condition("south", "low"), sideS)
+                                     .part(Selector.and().condition("west", "low"), sideW)
+                                     .part(Selector.and().condition("north", "tall"), sideTallN)
+                                     .part(Selector.and().condition("east", "tall"), sideTallE)
+                                     .part(Selector.and().condition("south", "tall"), sideTallS)
+                                     .part(Selector.and().condition("west", "tall"), sideTallW);
+    }
+
+    public static StateGen wallAll(String name, String texName) {
+        return wallGeneric(
+            name,
+            InheritingModelGen.wallPost(texName),
+            InheritingModelGen.wallSide(texName),
+            InheritingModelGen.wallSideTall(texName)
+        );
+    }
+
+    public static StateGen wallSided(String name, String bottom, String top, String side) {
+        return wallGeneric(
+            name,
+            InheritingModelGen.wallSidedPost(bottom, top, side),
+            InheritingModelGen.wallSidedSide(bottom, top, side),
+            InheritingModelGen.wallSidedSideTall(bottom, top, side)
+        );
+    }
+
+    public static StateGen wallColumn(String name, String end, String side) {
+        return wallSided(name, end, end, side);
+    }
+
+    private static StateGen slabGeneric(String name, String doubleName, ModelGen bottom, ModelGen top, ModelGen full) {
+        // No overload because we use cubeAll here instead of cubeBottomTop - this makes the difference
+        if (doubleName == null) {
+            // Generate custom double model
+            return VariantsBlockStateGen
+                       .variants("type=bottom", ModelInfo.create(name, bottom))
+                       .variant("type=top", ModelInfo.create(name + "_top", top))
+                       .variant("type=double", ModelInfo.create(name + "_double", full));
+        } else {
+            // Use existing model
+            return VariantsBlockStateGen
+                       .variants("type=bottom", ModelInfo.create(name, bottom))
+                       .variant("type=top", ModelInfo.create(name + "_top", top))
+                       .variant("type=double", ModelInfo.create(doubleName));
+        }
+    }
+
+    public static StateGen slabAll(String name, String doubleName, String texName) {
+        return slabGeneric(
+            name, doubleName,
+            InheritingModelGen.slabAll(texName),
+            InheritingModelGen.slabAllTop(texName),
+            doubleName == null ? InheritingModelGen.cubeAll(texName) : null
+        );
+    }
+
+    public static StateGen slabSided(String name, String doubleName, String bottom, String top, String side) {
+        return slabGeneric(
+            name, doubleName,
+            InheritingModelGen.slab(bottom, top, side),
+            InheritingModelGen.slabTop(bottom, top, side),
+            doubleName == null ? InheritingModelGen.cubeBottomTop(bottom, top, side) : null
+        );
+    }
+
+    public static StateGen slabColumn(String name, String doubleName, String end, String side) {
+        return slabSided(name, doubleName, end, end, side);
+    }
+
+    private static StateGen stairsGeneric(String name, ModelGen inner, ModelGen outer, ModelGen straight) {
+        boolean innerM = false; // Set to true after we supplied a modelgen, so we don't generate it twice
+        boolean outerM = false;
+        boolean stairsM = false;
+
+        VariantsBlockStateGen gen = VariantsBlockStateGen.variants();
+        int y = 270;
+        for (Direction dir : Direction.Type.HORIZONTAL) {
+            for (BlockHalf half : BlockHalf.values()) {
+                int x = half == BlockHalf.TOP ? 180 : 0;
+                String state = String.format("facing=%s,half=%s", dir.asString(), half.asString());
+
+                String iname = name + "_inner";
+                String oname = name + "_outer";
+
+                int yp = y == 0 ? 270 : y - 90;
+                int yn = y == 270 ? 0 : y + 90;
+
+                ModelInfo innerL = ModelInfo.create(iname, innerM ? null : inner).rotate(x, x == 180 ? y : yp).uvlock(true);
+                ModelInfo outerL = ModelInfo.create(oname, outerM ? null : outer).rotate(x, x == 180 ? y : yp).uvlock(true);
+                ModelInfo innerR = ModelInfo.create(iname).rotate(x, x == 180 ? yn : y).uvlock(true);
+                ModelInfo outerR = ModelInfo.create(oname).rotate(x, x == 180 ? yn : y).uvlock(true);
+                ModelInfo stairs = ModelInfo.create(name, stairsM ? null : straight).rotate(x, y).uvlock(true);
+
+                innerM = true;
+                outerM = true;
+                stairsM = true;
+
+                gen.variant(state + ",shape=straight", stairs);
+                gen.variant(state + ",shape=inner_left", innerL);
+                gen.variant(state + ",shape=inner_right", innerR);
+                gen.variant(state + ",shape=outer_left", outerL);
+                gen.variant(state + ",shape=outer_right", outerR);
+            }
+            if (y == 270) y = 0;
+            else y += 90;
+        }
+        return gen;
+    }
+
+    public static StateGen stairsAll(String name, String texName) {
+        return stairsGeneric(
+            name,
+            InheritingModelGen.stairsAllInner(texName),
+            InheritingModelGen.stairsAllOuter(texName),
+            InheritingModelGen.stairsAll(texName)
+        );
+    }
+
+    public static StateGen stairsSided(String name, String bottom, String top, String side) {
+        return stairsGeneric(
+            name,
+            InheritingModelGen.stairsInner(bottom, top, side),
+            InheritingModelGen.stairsOuter(bottom, top, side),
+            InheritingModelGen.stairs(bottom, top, side)
+        );
+    }
+
+    public static StateGen stairsColumn(String name, String end, String side) {
+        return stairsSided(name, end, end, side);
+    }
+}
