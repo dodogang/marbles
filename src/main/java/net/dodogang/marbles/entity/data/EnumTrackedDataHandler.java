225f8b655ffc5895aa24ddea4e4aa61071f4dff8
Daniel Orr <orrdaniel73@gmail.com>
2021-08-29 12:40:13 +0100

    optimise EnumTrackedDataHandler

--- src/main/java/net/dodogang/marbles/entity/data/EnumTrackedDataHandler.java
+++ src/main/java/net/dodogang/marbles/entity/data/EnumTrackedDataHandler.java
@@ -5,17 +5,17 @@ import net.minecraft.network.PacketByteBuf;
 
 public record EnumTrackedDataHandler<T extends Enum<T>>(Class<T> clazz) implements TrackedDataHandler<T> {
     @Override
-    public void write(PacketByteBuf buf, T variant) {
-        buf.writeString(variant.name());
+    public void write(PacketByteBuf buf, T value) {
+        buf.writeEnumConstant(value);
     }
 
     @Override
     public T read(PacketByteBuf buf) {
-        return T.valueOf(this.clazz, buf.readString());
+        return buf.readEnumConstant(this.clazz);
     }
 
     @Override
-    public T copy(T variant) {
-        return variant;
+    public T copy(T value) {
+        return value;
     }
 }
